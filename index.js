267b4773e023740486efc6fa87c2b907584fdc02
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-01-14 16:27:10 +0530

    create http

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-// const math = require("./math")
-const math = require("fs")
+// // const math = require("./math")
+// const math = require("fs")
 
-// console.log("Value of add is", math.add(2,3));
-// console.log("Value of add is", math.sub(2,3));
-console.log("Value of add is", math);
+// // console.log("Value of add is", math.add(2,3));
+// // console.log("Value of add is", math.sub(2,3));
+// console.log("Value of add is", math);
+
+const { log } = require("console");
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req, res) => {
+  //   console.log(req.headers);
+  console.log(`${Date.now()}: ${req.url} New req recived\n`);
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("HomePage");
+        break;
+
+      case "/about":
+        res.end(" I am about page of Utkarsh Sharma");
+        break;
+
+      case "/contact":
+        res.end("This is my contact megayt750@gmail.com");
+        break;
+
+      default:
+        res.end("404 not found");
+    }
+  });
+});
+
+myServer.listen(8000, () => {
+  console.log("Server started");
+});
