8386895c58a62d15b3daee350b529757a2819cbc
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-01-19 02:04:48 +0530

    created new routes

--- index.js
+++ index.js
@@ -3,10 +3,51 @@ const users = require("./MOCK_DATA.json");
 const PORT = 8000;
 const app = express();
 
-// Routes
+app.use(express.urlencoded({ extended: false }));
 app.get("/users", (req, res) => {
+  const html = `
+          <ul> 
+          ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+          </ul>
+      `;
+  res.send(html);
+});
+// Routes
+app.get("/api/users", (req, res) => {
   return res.json(users);
 });
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    return res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    return res.json({ status: "Pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  // Create new user
+  const body = req.body;
+  console.log("Body", body);
+  return res.json({ status: "Pending" });
+});
+
+// app.patch("/api/users/:id", (req, res) => {
+//   // Edit new user with ID
+//   return res.send({ status: "Pending" });
+// });
+
+// app.delete("/api/users/:id", (req, res) => {
+//   // Delete user with ID
+//   return res.send({ status: "Pending" });
+// });
+
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
 });
